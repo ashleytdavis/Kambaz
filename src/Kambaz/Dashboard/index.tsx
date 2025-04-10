f408d5e89c63022791ef70c5972d1de26ed3c251
Ashley Davis <ashley921davis@gmail.com>
2025-04-10 10:13:04 -0400

    frontend-changes

--- src/Kambaz/Dashboard/index.tsx
+++ src/Kambaz/Dashboard/index.tsx
@@ -3,6 +3,8 @@ import { Button, Card, Col, Row, FormControl } from "react-bootstrap";
 import FacultyContent from "../FacultyContent";
 import { useState } from "react";
 import StudentContent from "../StudentContent";
+import { FaFilePen } from "react-icons/fa6";
+import { FaRegFolder } from "react-icons/fa";
 
 export default function Dashboard(
     { courses, addNewCourse, deleteCourse, updateCourse, enrolling, setEnrolling, updateEnrollment }: {
@@ -11,6 +13,7 @@ export default function Dashboard(
         updateEnrollment: (courseId: string, enrolled: boolean) => void;
     }) {
     const [course, setCourse] = useState<any>({});
+    const [courseAssignments, setCourseAssignments] = useState<any>([])
 
     const handleAddCourse = () => {
         addNewCourse(course);
@@ -23,9 +26,9 @@ export default function Dashboard(
     };
 
     return (
-        <div id="wd-dashboard">
+        <div id="wd-dashboard" className="px-5 py-4">
             <div>
-                <h1 id="wd-dashboard-title">Dashboard
+                <h1 id="wd-dashboard-title" className="fw-bold">Dashboard
                     <button onClick={() => setEnrolling(!enrolling)} className="float-end btn btn-primary" >
                         {enrolling ? "My Courses" : "All Courses"}
                     </button>
@@ -51,34 +54,46 @@ export default function Dashboard(
                 <div id="wd-dashboard-courses">
                     <Row xs={1} md={5} className="g-4">
                         {courses.map((course) => (
-                            <Col key={course._id} className="wd-dashboard-course" style={{ width: "300px" }}>
+                            <Col key={course._id} className="wd-dashboard-course " style={{ width: "300px" }}>
                                 <Card>
                                     <Link to={`/Kambaz/Courses/${course._id}/Home`}
                                         className="wd-dashboard-course-link text-decoration-none text-dark">
                                         <Card.Img src={course.image_filepath} variant="top" width="100%" height={160} />
                                         <Card.Body className="card-body">
-                                            <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                                            <Card.Title className="wd-dashboard-course-title text-fit overflow-hidden fw-semibold">
                                                 {course.name} </Card.Title>
-                                            <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                                {course.description} </Card.Text>
-                                            <div className="d-flex justify-content-between">                                                <FacultyContent>
-                                                <Button id="wd-edit-course-click"
-                                                    onClick={(event) => {
-                                                        event.preventDefault();
-                                                        setCourse(course);
-                                                    }}
-                                                    className="btn btn-danger me-2">
-                                                    Edit
-                                                </Button>
-                                                <Button onClick={(event) => {
-                                                    event.preventDefault();
-                                                    deleteCourse(course._id);
-                                                }} className="btn btn-danger float-end"
-                                                    id="wd-delete-course-click">
-                                                    Delete
-                                                </Button>
+                                            <div className="d-flex justify-content-between align-items-center">
+                                                <FaRegFolder />
+                                                <Link to={`/Kambaz/Courses/${course._id}/Assignments`} className="text-decoration-none text-dark hover-red">
+                                                    <FaFilePen />
+                                                </Link>
 
-                                            </FacultyContent>
+                                            </div>
+                                            <Card.Text className="fw-bold">Due</Card.Text>
+                                            {courseAssignments.length > 0 ?
+                                                courseAssignments.map((assignment: any) => (
+                                                    null
+                                                    // here is where we would map the assignments of each course, sorted by dueDate
+                                                )) : <p>None</p>}
+                                            <div className="d-flex justify-content-between">
+                                                <FacultyContent>
+                                                    <Button id="wd-edit-course-click"
+                                                        onClick={(event) => {
+                                                            event.preventDefault();
+                                                            setCourse(course);
+                                                        }}
+                                                        className="btn btn-danger me-2">
+                                                        Edit
+                                                    </Button>
+                                                    <Button onClick={(event) => {
+                                                        event.preventDefault();
+                                                        deleteCourse(course._id);
+                                                    }} className="btn btn-danger float-end"
+                                                        id="wd-delete-course-click">
+                                                        Delete
+                                                    </Button>
+
+                                                </FacultyContent>
                                                 <StudentContent>
                                                     {enrolling && (
                                                         <button onClick={(event) => {
